2226a0ef27937474eb48d2c9ab6258886eab63c4
IhorChepak <chepak_lex@ukr.net>
2025-04-18 12:00:16 +0300

    7-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(evt) {
+   
+    evt.preventDefault()
+
+    const {email, password}=evt.currentTarget.elements;
+
+    if(email.value===''|| password.value===''){
+        alert('Усі поля повинні бути заповнені!');
+        return
+    }
+
+const data = {
+    email: email.value,
+    password: password.value
+}
+console.log(data);
+form.reset()
+}
